ffa7638b2e67da3c8e606625001050c4f3bdac3d
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-08-02 22:36:44 +0800

    initial commit

---
+++ Array_and_Pointer/Problem_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num[10];
+    int transformEven;
+    int transformOdd;
+    cout << "Enter 10 numbers:\n";
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "#" << i + 1 << ": ";
+        cin >> num[i];
+    }
+
+    cout << "\nOdd-Even Transformation:" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        if ((num[i] % 2) == 0)
+        {
+            transformEven = num[i] - 2;
+            cout << transformEven << " ";
+        }
+        else
+        {
+            transformOdd = num[i] + 2;
+            cout << transformOdd << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
