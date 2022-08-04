713e5cdc8cbaf2d07b2e6ef68a6c36e02d19d5cf
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-08-04 19:29:02 +0800

    added function definition

--- FunctionProblems/Problem_12.cpp
+++ FunctionProblems/Problem_12.cpp
@@ -16,6 +16,33 @@ int main()
 
     cout << average << " on a 4 point scale is " << qualityPoints(average) << "\n";
     cout << endl;
-    
+
     return 0;
+}
+
+int qualityPoints(int gwa)
+{
+    int equivalentPoints;
+
+    if (gwa >= 90 && gwa <= 100)
+    {
+        equivalentPoints = 4;
+    }
+    else if (gwa >= 80 && gwa <= 89)
+    {
+        equivalentPoints = 3;
+    }
+    else if (gwa >= 70 && gwa <= 79)
+    {
+        equivalentPoints = 2;
+    }
+    else if (gwa >= 60 && gwa <= 69)
+    {
+        equivalentPoints = 1;
+    }
+    else if (gwa < 60)
+    {
+        equivalentPoints = 0;
+    }
+    return equivalentPoints;
 }
