d06b2d54da07df61beb8ffa4b300a2cc29482dd1
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-08-06 15:12:39 +0800

    initial commit

--- Structure_and_File_Handling/Problem_13.cpp
+++ Structure_and_File_Handling/Problem_13.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+// #include <string>
 using namespace std;
+
+void EateryName()
+{
+    cout << "\t\tKamay Kainan Restaurants " << endl;
+    cout << "\t\tQuezon City" << endl;
+}
+
+void Display();
+
+struct Customer
+{
+    // Customer's profile
+    string customerName;
+    int numOfCompanions;
+    // Order Details
+    int itemNum;
+    string itemDescription;
+    int price;
+    int quantity;
+};
 
 int main()
 {
+    EateryName();
+    struct Customer customer;
+    int customerNum;
+    cout << "\nEnter Customer's Name: ";
+    getline(cin, customer.customerName);
 
+    cout << "\nEnter Number of Companions: ";
+    cin >> customer.numOfCompanions;
+
+    cout << "\nOrder Details";
+
+    for (int i = 0; i < customer.numOfCompanions; i++)
+    {
+        cout << "\nEnter Item Number: ";
+        cin >> customer.itemNum;
+
+        cout << "\nEnter Item Description: ";
+        cin >> customer.itemDescription;
+
+        cout << "\nEnter Number of Quantity: ";
+        cin >> customer.quantity;
+
+        cout << "\nPrice: ";
+        cin >> customer.price;
+    }
     return 0;
 }
