a43b497aedf3380a909be50deb206b57c48f9db7
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-08-07 14:29:49 +0800

    format file

--- Array_and_Pointer/Problem_1.cpp
+++ Array_and_Pointer/Problem_1.cpp
@@ -4,30 +4,25 @@ BSIT 1 - 2
 PRACTICE PROBLEM #1: ODD UP, EVEN DOWN
 */
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
     int num[10];
     int transformEven;
     int transformOdd;
     cout << "Enter 10 numbers:\n";
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         cout << "#" << i + 1 << ": ";
         cin >> num[i];
     }
 
     cout << "\nOdd-Even Transformation:" << endl;
-    for (int i = 0; i < 10; i++)
-    {
-        if ((num[i] % 2) == 0)
-        {
+    for (int i = 0; i < 10; i++) {
+        if ((num[i] % 2) == 0) {
             transformEven = num[i] - 2;
             cout << transformEven << " ";
-        }
-        else
-        {
+        } else {
             transformOdd = num[i] + 2;
             cout << transformOdd << " ";
         }
