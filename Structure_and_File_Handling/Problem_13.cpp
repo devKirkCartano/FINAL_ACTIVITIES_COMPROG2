2cc2229854b6cc5e791bcef5ceba4fc42fc6a278
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-08-09 18:27:33 +0800

    change flow of program

--- Structure_and_File_Handling/Problem_13.cpp
+++ Structure_and_File_Handling/Problem_13.cpp
@@ -11,9 +11,9 @@ const int year = 2022;
 
 void DisplayName()
 {
-    cout << "\n\t\t\t      KAMAY KAINAN RESTAURANTS " << endl;
-    cout << "\t\t\t\t    QUEZON CITY" << endl;
-    cout << " \n\n  " << asterisk << endl;
+	cout << "\n\t\t\t      KAMAY KAINAN RESTAURANTS " << endl;
+	cout << "\t\t\t\t    QUEZON CITY" << endl;
+	cout << " \n\n  " << asterisk << endl;
 }
 
 void DisplayCashier()
@@ -26,85 +26,122 @@ void CustomerReceipt();
 
 struct CustomerProfile
 {
-    string customerName;
-    int numOfCompanions;
+	string customerName;
+	int numOfCompanions;
 };
 
 struct OrderDetails
 {
-    string itemNum;
-    string itemDescription;
-    int price;
-    int quantity;
+	string itemNum;
+	string itemDescription;
+	int price;
+	int quantity;
 	double amount;
 };
 
 int main()
 {
 
-    DisplayName();
-    DisplayCashier();
-    struct CustomerProfile customer;
-    int customerNum;
-    string moreTransaction;
+	DisplayName();
+	DisplayCashier();
+	struct CustomerProfile customer;
+	int customerNum;
+	string moreTransaction;
+	string entry;
+	// Validate month input
+	do
+	{
+		cout << "\n  Enter month of the year [ 1 - 12 ]: ";
+		cin >> month;
+	} while (month != "1" && month != "2" && month != "3" && month != "4" && month != "5" && month != "6" && month != "7" && month != "8" && month != "9" && month != "10" && month != "11" && month != "12");
 
-    // Validate month input
-    do
-    {
-        cout << "\n  Enter month of the year [ 1 - 12 ]: ";
-        cin >> month;
-    } while (month != "1" && month != "2" && month != "3" && month != "4" && month != "5" && month != "6" && month != "7" && month != "8" && month != "9" && month != "10" && month != "11" && month != "12");
+	// Validate date input
+	do
+	{
+		cout << "\n  Enter the date [ 1 - 31 ]: ";
+		cin >> day;
+	} while (day != "1" && day != "2" && day != "3" && day != "4" && day != "5" && day != "6" && day != "7" && day != "8" && day != "9" && day != "10" && day != "11" && day != "12" && day != "13" && day != "14" && day != "15" && day != "16" && day != "17" && day != "18" && day != "19" && day != "20" && day != "21" && day != "22" && day != "23" && day != "24" && day != "25" && day != "26" && day != "27" && day != "28" && day != "29" && day != "30" && day != "31");
 
-    // Validate date input
-    do
-    {
-        cout << "\n  Enter the date [ 1 - 31 ]: ";
-        cin >> day;
-    } while (day != "1" && day != "2" && day != "3" && day != "4" && day != "5" && day != "6" && day != "7" && day != "8" && day != "9" && day != "10" && day != "11" && day != "12" && day != "13" && day != "14" && day != "15" && day != "16" && day != "17" && day != "18" && day != "19" && day != "20" && day != "21" && day != "22" && day != "23" && day != "24" && day != "25" && day != "26" && day != "27" && day != "28" && day != "29" && day != "30" && day != "31");
+	do
+	{
+		system("cls"); // this clear screen function only works in windows operating system
+		DisplayName();
+		DisplayCashier();
+		cin.ignore(1);
 
-    do
-    {
-        system("cls"); // this clear screen function only works in windows operating system
-        DisplayName();
-        DisplayCashier();
-        cin.ignore(1);
-
-        cout << " \n  " << line << endl;
-        cout << "\n\t\t\t    " << design << " CUSTOMER PROFILE " << design << endl;
-        cout << "\n  Enter Customer's Name: ";
-        getline(cin, customer.customerName);
-
-        cout << "\n  Enter Number of Companions: ";
-        cin >> customer.numOfCompanions;
-
-		// Declare array of structures 
-		struct OrderDetails order[customer.numOfCompanions];
-        cout << " \n  " << line << endl;
-        cout << "\n\t\t\t     " << design << " ORDER DETAILS " << design << endl;
-                for (int i = 0; i < customer.numOfCompanions; i++)
-                {
-                    cout << "\n  Enter Item Number: ";
-                    cin >> order[i].itemNum;
-					cin.ignore(1);
-                    cout << "\n  Enter Item Description: ";
-                    getline(cin,order[i].itemDescription);
-
-                    cout << "\n  Enter Number of Quantity: ";
-                    cin >> order[i].quantity;
-
-                    cout << "\n  Price: ";
-                    cin >> order[i].price;
-
-                    cout << " \n  " << line << endl;
-                }
-        system("cls");
-        DisplayName();
-        DisplayCashier();
 		cout << " \n  " << line << endl;
-        cout << "\n\t\t\t      KAMAY KAINAN RESTAURANTS " << endl;
-        cout << "\n\t\t\t          OFFICIAL RECEIPT\n\n";
+		cout << "\n\t\t\t    " << design << " CUSTOMER PROFILE " << design << endl;
+		cout << "\n  Enter Customer's Name: ";
+		getline(cin, customer.customerName);
 
-		cout << "\n  Customer Name: "<< customer.customerName << endl;
+		cout << "\n  Enter Number of Companions: ";
+		cin >> customer.numOfCompanions;
+
+		// Declare array of structures
+		cout << " \n  " << line << endl;
+		cout << "\n\t\t\t     " << design << " ORDER DETAILS " << design << endl;
+		struct OrderDetails order[customer.numOfCompanions];
+
+		for (int i = 0; i < customer.numOfCompanions; i++)
+		{
+			cout << "\n  Order # " << i + 1 << endl;
+			do
+			{
+				cout << "\n  Enter Item Number: ";
+				cin >> order[i].itemNum;
+				cin.ignore(1);
+			} while (order[i].itemNum == "");
+
+			do
+			{
+				cout << "\n  Enter Item Description: ";
+				getline(cin, order[i].itemDescription);
+			} while (order[i].itemDescription == "");
+
+			do
+			{
+				cout << "\n  Enter Quantity: ";
+				cin >> order[i].quantity;
+				cin.ignore(1);
+
+			} while (order[i].quantity <= 0);
+
+			do
+			{
+				cout << "\n  Enter Item Price: ";
+				cin >> order[i].price;
+				cin.ignore(1);
+			} while (order[i].price <= 0);
+
+
+			if (i < customer.numOfCompanions - 1)
+			{
+				do
+				{
+					cout << "\n  More entry [Y/N]? : ";
+					cin >> entry;
+					cin.ignore(1);
+					if (entry != "Y" && entry != "y" && entry != "N" && entry != "n")
+					{
+						cout << "\n [Please answer with Y or N] \n";
+					}
+				} while (entry != "Y" && entry != "y" && entry != "N" && entry != "n");
+
+				if (entry == "N" || entry == "n")
+				{
+					customer.numOfCompanions = i + 1;
+					break;
+				}
+			}
+		}
+		system("cls");
+		DisplayName();
+		DisplayCashier();
+		cout << " \n  " << line << endl;
+		cout << "\n\t\t\t      KAMAY KAINAN RESTAURANTS " << endl;
+		cout << "\n\t\t\t          OFFICIAL RECEIPT\n\n";
+
+		cout << "\n  Customer Name: " << customer.customerName << endl;
 
 		// Calculate amount of each item
 		for (int i = 0; i < customer.numOfCompanions; i++)
@@ -115,16 +152,17 @@ int main()
 		cout << "\n  Item No.\tItem Description\tQty\t\tPrice\tAmount \n\n";
 		for (int i = 0; i < customer.numOfCompanions; i++)
 		{
-			cout << "  " << order[i].itemNum  << "\t\t" << order[i].itemDescription << "\t\t\t" << order[i].quantity  << "\t\t"<< order[i].price << "\t" << order[i].amount << endl;
+			cout << "  " << order[i].itemNum << "\t\t" << order[i].itemDescription << "\t\t\t" << order[i].quantity << "\t\t" << order[i].price << "\t" << order[i].amount << endl;
 		}
+
 		cout << "\n  More Transaction?: ";
-        cin >> moreTransaction;
+		cin >> moreTransaction;
 
-        if (moreTransaction == "N" || moreTransaction == "n")
-        {
-            break;
-        }
-    } while (moreTransaction == "Y" || moreTransaction == "y");
-    return 0;
+		if (moreTransaction == "N" || moreTransaction == "n")
+		{
+			break;
+		}
+	} while (moreTransaction == "Y" || moreTransaction == "y");
+
+	return 0;
 }
-
